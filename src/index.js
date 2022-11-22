aaa02d89f1a9cb0cebb7c4968b7859f2755aabc5
Kamil Szafrański <107311803+KamilSzafranski@users.noreply.github.com>
2022-11-22 19:16:23 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./css/styles.css";
 import { fetchCountries } from "./fetchCountries";
 import debounce from "lodash.debounce";
+import Notiflix from "notiflix";
 
 const DEBOUNCE_DELAY = 300;
 
@@ -8,21 +9,78 @@ const input = document.querySelector("input");
 const list = document.querySelector("ul.country-list");
 const info = document.querySelector("div.country-info");
 
-clearList = () => (list.innerHTML = "");
 
-renderList = data => {
-  clearList();
-  const markup = data
-    .map(element => {
-      return `<li><p data-flag=${element.flags.svg}>${element.name}</p></li>`;
-    })
-    .join("");
+const changeBorderList = () => {
+  input.addEventListener("blur", event => {
+    list.style.border = "1px solid #212121";
+    list.style.borderTop = "none";
+  })
+    input.addEventListener("focus", event => {
+      list.style.border = "2px solid mediumseagreen";
+      list.style.borderTop = "none";
+    });
+  
 
-  list.insertAdjacentHTML("beforeend", markup);
+}
+const clearList = () => {
+  list.innerHTML = "";
+  list.style.transform = "translateY(-500%)";
 };
 
+const clearInfo = () => {
+  info.innerHTML = "";
+};
+
+const renderList = data => {
+  clearList();
+  clearInfo();
+  const markup = data
+    .map(element => {
+      return `<li class="country__item"><span  class="flag" style="background-image:url('${element.flags.svg}')"></span><span class="country">${element.name}</span></li>`;
+    })
+    .join("");
+  list.insertAdjacentHTML("beforeend", markup);
+
+  setTimeout(() => {
+    list.style.transform = "translateY(0%)";
+  }, 300);
+};
+
+const renderInfo = data => {
+  clearInfo();
+  clearList();
+  const languages = data[0].languages
+    .map(element => {
+      return element.name;
+    })
+    .join(",");
+
+  const markup = data
+    .map(element => {
+      return `<p><span  class="flag--Big" style="background-image:url('${element.flags.svg}')"></span> <span class="country--Big">${element.name}</span></p>
+  <p>Capital:<span>${element.capital}</span></p>
+  <p>Population:<span>${element.population}</span></p>
+  <p>Languages:<span>${languages}</span></p>`;
+    })
+    .join("");
+  info.insertAdjacentHTML("beforeend", markup);
+};
 const deb = debounce(event => {
-  fetchCountries(`${event.target.value}`).then(data => renderList(data));
+  const inputValue = event.target.value.trim();
+  fetchCountries(`${inputValue}`)
+    .then(data => {
+      if (data.length < 11 && data.length >= 2) return renderList(data);
+      if (data.length < 2) return renderInfo(data);
+      return Notiflix.Notify.info(
+        "Too many matches found. Please enter a more specific name."
+      );
+    })
+    .catch(error => {
+      clearList();
+      console.error(error);
+      Notiflix.Notify.failure("Oops, there is no country with that name");
+    });
 }, DEBOUNCE_DELAY);
 
 input.addEventListener("input", deb);
+changeBorderList();
